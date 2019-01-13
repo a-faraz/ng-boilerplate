98b2eded70b0e84031ae1d6fabe14e42bd33028b
Anjum Faraz <anjumfaraz10@gmail.com>
2019-01-13 12:37:25 -0600

    refactor: Add components module refactor file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ComponentsModule } from './components';
+import { AppComponent } from './containers';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    ComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/components.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+const components = [];
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: components,
+  exports: components
+})
+export class ComponentsModule {}
